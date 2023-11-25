c9c7fc031402933d953d27b324932744c4664cf5
Zilant1337 <grigorenko_p@inbox.ru>
2023-11-25 14:51:35 +0300

    KISA RISUETSA AAAAAAAAAAAAAA

--- hw3_grigorenko_pavel_09-132.py
+++ hw3_grigorenko_pavel_09-132.py
@@ -3,16 +3,20 @@ import numpy as np
 import re
 import tkinter as tk
 import matplotlib.pyplot as plt
+import locale
 
 pointList=[]
 bodyCurveList=[]
+eyesCurveList=[]
 tailCurveList=[]
+irisesCurveList=[]
 
 root =tk.Tk()
 screenWidth = root.winfo_screenwidth()/3
 fig = plt.figure()
 image= np.full((int(screenWidth),int(screenWidth),3),[19,24,98],dtype=np.float32)
-color=[0,0,0]
+bodyColor=[0,0,0]
+eyeColor=[255,255,51]
 class Point:
     x=int
     y=int
@@ -98,16 +102,62 @@ def AddCurve(point0,point1,point2):
     pointList.append(point2)
     bodyCurveList.append(BezierCurve(point0,point1,point2))
 
+def ReadObjFile(file):
+    text=[]
+    text=file.readlines()
+    return text
+
 def Draw(points,color):
     for i in points:
         X = np.matrix([[i[0]], [i[1]], [1]])
+        F = np.matrix([[-1, 0, 0],
+                       [0, 1, 0], [0, 0, 1]])
         R = np.matrix([[math.cos(math.radians(-90)), -1 * math.sin(math.radians(-90)), 0],
                        [math.sin(math.radians(-90)), math.cos(math.radians(-90)), 0], [0, 0, 1]])
+        F= np.matrix([[1, 0, 0],
+                       [0, -1, 0], [0, 0, 1]])
+        flippedX= np.matmul(F,X)
         rotatedX = np.matmul(R, X)
-        image[int(rotatedX[0][0]),int(rotatedX[1][0])]=color
+        flippedX = np.matmul(F, rotatedX)
+        image[int(flippedX[0][0]),int(flippedX[1][0])]=color
+def ParseBodyBezierFile(text):
+    for i in text:
+        tempText = i.split(" ")
+        points=[]
+        for j in tempText:
+            individualCoords=[]
+            individualCoords= j.split(",")
+            if individualCoords[0] == "\n":
+                break
 
 
+            points.append((int(round(float(individualCoords[0]))),int(round(float(individualCoords[1])))))
+        bezier = make_bezier(points)
+        points1 = bezier(ts)
+        bodyCurveList.append(points1)
 
+def ParseEyesBezierFile(text):
+    for i in text:
+        tempText = i.split(" ")
+        points=[]
+        for j in tempText:
+            individualCoords=[]
+            individualCoords= j.split(",")
+            if individualCoords[0] == "\n":
+                break
+
+
+            points.append((int(round(float(individualCoords[0]))),int(round(float(individualCoords[1])))))
+        bezier = make_bezier(points)
+        points1 = bezier(ts)
+        eyesCurveList.append(points1)
+
+def DrawBody(color):
+    for i in bodyCurveList:
+        Draw(i,color)
+def DrawEyes(color):
+    for i in eyesCurveList:
+        Draw(i,color)
 
 ts = [t/screenWidth for t in range(int(screenWidth+1))]
 #
@@ -125,11 +175,21 @@ ts = [t/screenWidth for t in range(int(screenWidth+1))]
 # bezier = make_bezier(xys)
 # points.extend(bezier(ts))
 
-xys = [(254, 690), (438, 518), (704, 687)]
-bezier = make_bezier(xys)
-points = bezier(ts)
+f1= open("BodyBezierpoints.txt","r")
+bodyText=ReadObjFile(f1)
+f2= open("EyesBezierpoints.txt","r")
+eyesText=ReadObjFile(f2)
 
-Draw(points,color)
+ParseBodyBezierFile(bodyText)
+ParseEyesBezierFile(eyesText)
+DrawBody(bodyColor)
+DrawEyes(eyeColor)
+
+# xys = [(254, 690), (438, 518), (704, 687)]
+# bezier = make_bezier(xys)
+# points = bezier(ts)
+
+# Draw(points,color)
 
 im=plt.imshow(image.astype('uint8'))
 
